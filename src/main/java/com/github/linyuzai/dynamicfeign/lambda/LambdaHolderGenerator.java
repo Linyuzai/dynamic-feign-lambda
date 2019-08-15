258ada287ddf4d6fbd65b2dc12b0a96d9c139828
thz <t88510930>
2019-08-15 09:57:44 +0800

    LambdaHolder生成器指定class生成

--- src/main/java/com/github/linyuzai/dynamicfeign/lambda/LambdaHolderGenerator.java
+++ src/main/java/com/github/linyuzai/dynamicfeign/lambda/LambdaHolderGenerator.java
@@ -4,40 +4,78 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
+import javax.tools.*;
 import java.io.*;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 
 public class LambdaHolderGenerator {
 
-    //private static String CLASSES_PATH = File.separator + "dynamic_feign" + File.separator;
     private static Logger logger = LoggerFactory.getLogger(LambdaHolderGenerator.class);
 
     private static JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 
+    private static List<String> classpath = new ArrayList<>();
+
+    private static List<String> sourcepath = new ArrayList<>();
+
     public static String absoluteProjectPath = null;
+
+    public static void addClasspath(String... classpath) {
+        LambdaHolderGenerator.classpath.addAll(Arrays.asList(classpath));
+    }
+
+    public static void addSourcepath(String... sourcepath) {
+        LambdaHolderGenerator.sourcepath.addAll(Arrays.asList(sourcepath));
+    }
+
+    public static void buildClasspath(File file) {
+        if (file == null) {
+            return;
+        }
+        if (file.isDirectory()) {
+            addClasspath(file.getAbsolutePath() + File.separator + "*");
+            File[] fileList = file.listFiles();
+            if (fileList != null) {
+                for (File f : fileList) {
+                    buildClasspath(f);
+                }
+            }
+        }
+    }
 
     private static List<Class<?>> compile(List<File> files) {
         List<Class<?>> classes = new ArrayList<>();
+        //String cp = String.join(File.pathSeparator, classpath);
+        //String sp = String.join(File.pathSeparator, sourcepath);
+        //Iterable<String> options = Arrays.asList("-classpath", cp, "-sourcepath", sp);
+        //System.out.println(options);
+        List<File> classpathFiles = new ArrayList<>();
+        classpath.stream().map(File::new).forEach(classpathFiles::add);
         for (File file : files) {
-            StandardJavaFileManager stdManager = compiler.getStandardFileManager(null, null, null);
-            try (MemoryJavaFileManager manager = new MemoryJavaFileManager(stdManager);
-                 FileInputStream fis = new FileInputStream(file); InputStreamReader isr = new InputStreamReader(fis);
+            String content = null;
+            try (FileInputStream fis = new FileInputStream(file); InputStreamReader isr = new InputStreamReader(fis);
                  BufferedReader br = new BufferedReader(isr)) {
                 StringBuilder builder = new StringBuilder();
                 String line;
                 while ((line = br.readLine()) != null) {
                     builder.append(line).append("\n");
                 }
-                JavaFileObject javaFileObject = manager.makeStringSource(file.getName(), builder.toString());
+                content = builder.toString();
+                if (!content.contains("@FeignClient(")) {
+                    continue;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (content == null) {
+                continue;
+            }
+            try (StandardJavaFileManager stdManager = compiler.getStandardFileManager(null, null, null);
+                 MemoryJavaFileManager manager = new MemoryJavaFileManager(stdManager)) {
+                stdManager.setLocation(StandardLocation.CLASS_PATH, classpathFiles);
+                JavaFileObject javaFileObject = manager.makeStringSource(file.getName(), content);
                 JavaCompiler.CompilationTask task = compiler.getTask(null, manager, null, null, null, Collections.singletonList(javaFileObject));
                 if (task.call()) {
                     Map<String, byte[]> results = manager.getClassBytes();
@@ -64,9 +102,14 @@ public class LambdaHolderGenerator {
     public static void generate(String packageName) {
         try {
             String projectPath = absoluteProjectPath == null ? new File("").getCanonicalPath() : absoluteProjectPath;
-            String path = projectPath +
-                    File.separator + "src" + File.separator + "main" +
-                    File.separator + "java" + File.separator + packageName.replaceAll("\\.", Matcher.quoteReplacement(File.separator));
+            String sourcePath = projectPath +
+                    File.separator + "src" +
+                    File.separator + "main" +
+                    File.separator + "java";
+            //buildClasspath(new File(sourcePath));
+            String path = sourcePath +
+                    File.separator +
+                    packageName.replaceAll("\\.", Matcher.quoteReplacement(File.separator));
             generateFromSource(path, path, packageName);
         } catch (IOException e) {
             e.printStackTrace();
@@ -106,8 +149,8 @@ public class LambdaHolderGenerator {
         for (Class<?> cls : classes) {
             for (Method method : cls.getMethods()) {
                 content.append(generateClass(method));
-                content.append(generateAddMethod(method, "addMethodUrl", ", String url"));
-                content.append(generateRemoveMethod(method, "removeMethodUrl"));
+                content.append(generateAddMethod(method));
+                content.append(generateRemoveMethod(method));
             }
         }
         content.append("}");
@@ -133,23 +176,23 @@ public class LambdaHolderGenerator {
         return "    @FunctionalInterface\n"
                 + "    public interface " + generateLambdaHolderClassName(method) + "<T" + (returnClass == void.class ? "" : ", R") + "> extends java.io.Serializable {\n"
                 + "        " + returnClassName + " apply(T t" + generateParameterTypes(method) + ");\n"
-                + "    }\n";
+                + "    }\n\n";
     }
 
-    public static String generateAddMethod(Method method, String methodName, String extraParam) {
+    public static String generateAddMethod(Method method) {
         Class<?> returnClass = method.getReturnType();
-        return "    public static <T> boolean " + methodName + "(" + generateLambdaHolderClassName(method) + "<T" + (returnClass == void.class ? "" : ", ?") + ">" + " lambda, String url) {\n"
+        return "    public static <T> boolean addMethodUrl(" + generateLambdaHolderClassName(method) + "<T" + (returnClass == void.class ? "" : ", ?") + ">" + " lambda, String url) {\n"
                 + "        com.github.linyuzai.dynamicfeign.lambda.SerializedLambda serializedLambda = com.github.linyuzai.dynamicfeign.lambda.SerializedLambda.resolve(lambda);\n"
-                + "        return com.github.linyuzai.dynamicfeign.mapper.DynamicFeignClientMapper." + methodName + "(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);\n"
-                + "    }\n";
+                + "        return com.github.linyuzai.dynamicfeign.mapper.DynamicFeignClientMapper.addMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName(), url);\n"
+                + "    }\n\n";
     }
 
-    public static String generateRemoveMethod(Method method, String methodName) {
+    public static String generateRemoveMethod(Method method) {
         Class<?> returnClass = method.getReturnType();
-        return "    public static <T> boolean " + methodName + "(" + generateLambdaHolderClassName(method) + "<T" + (returnClass == void.class ? "" : ", ?") + ">" + " lambda) {\n"
+        return "    public static <T> boolean removeMethodUrl(" + generateLambdaHolderClassName(method) + "<T" + (returnClass == void.class ? "" : ", ?") + ">" + " lambda) {\n"
                 + "        com.github.linyuzai.dynamicfeign.lambda.SerializedLambda serializedLambda = com.github.linyuzai.dynamicfeign.lambda.SerializedLambda.resolve(lambda);\n"
                 + "        return com.github.linyuzai.dynamicfeign.mapper.DynamicFeignClientMapper.removeMethodUrl(serializedLambda.getImplClass(), serializedLambda.getImplMethodName());\n"
-                + "    }\n";
+                + "    }\n\n";
     }
 
     public static String generateLambdaHolderClassName(Method method) {
@@ -176,5 +219,50 @@ public class LambdaHolderGenerator {
 
     private static File getFile(String generatePath) {
         return new File(generatePath, "LambdaHolder.java");
+    }
+
+    //********************************************************************************
+
+    public static void generateFromBuild() {
+        generateFromBuild("build" +
+                File.separator + "classes" +
+                File.separator + "java" +
+                File.separator + "main");
+    }
+
+    public static void generateFromBuild(String path) {
+        try {
+            String projectPath = new File(absoluteProjectPath == null ?
+                    new File("").getCanonicalPath() : absoluteProjectPath)
+                    .getAbsolutePath();
+            String buildPath = projectPath + File.separator + path;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void filterClasses() {
+
+    }
+
+    //*************************************************************************
+
+    public static void generate(String packageName, Class<?>... classes) {
+        try {
+            String projectPath = new File(absoluteProjectPath == null ?
+                    new File("").getCanonicalPath() : absoluteProjectPath)
+                    .getAbsolutePath();
+            String sourcePath = projectPath +
+                    File.separator + "src" +
+                    File.separator + "main" +
+                    File.separator + "java";
+            String path = sourcePath +
+                    File.separator +
+                    packageName.replaceAll("\\.", Matcher.quoteReplacement(File.separator));
+            generateFromClasses(path, packageName, filterClasses(Arrays.asList(classes)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
